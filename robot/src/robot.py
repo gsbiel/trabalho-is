3ff9339bb9fde9182b843f1716d890a054d113b4
gabriel-gaspar <gabrielgaspar@wine.com.br>
2021-08-28 19:20:05 -0300

    [robot] parameterizes with configuration file

--- robot/src/robot.py
+++ robot/src/robot.py
@@ -4,6 +4,8 @@ from google.protobuf.empty_pb2 import Empty
 from is_msgs.common_pb2 import Position
 import time
 from loguru import logger
+import os
+import json
 
 class Robot():
     def init(self,id,x,y):
@@ -21,6 +23,10 @@ class Robot():
     def get_position(self):
         return self.pos_x,self.pos_y
 
+def read_configuration(config_path):
+  with open(config_path) as f:
+    return json.load(f)
+
 def get_position(Empty, ctx):
     logger.info("get_position() has been called.")
     position = Position()
@@ -35,23 +41,32 @@ def set_position(Position, ctx):
     robot.set_position(x=Position.x, y=Position.y)
     return Empty()
 
+config_path = os.getenv('CONFIG_PATH', './conf/config.json')
+configs = read_configuration(config_path)
+
+AMQ_HOST = os.getenv('AMQ_HOST', configs["amq_host"])
+TOPIC_TO_CONTROL_ROBOT = os.getenv('TOPIC_TO_CONTROL_ROBOT', configs["topic_to_control_the_robot"])
+TOPIC_TO_LOCATE_ROBOT = os.getenv('TOPIC_TO_LOCATE_ROBOT', configs["topic_to_locate_the_robot"]) 
+START_ROBOT_POSITION_X = os.getenv('START_ROBOT_POSITION_X', configs["start_robot_in_position_x"]) 
+START_ROBOT_POSITION_Y = os.getenv('START_ROBOT_POSITION_Y', configs["start_robot_in_position_y"])
+
 robot = Robot() 
 robot.id = 1
-robot.pos_x = 1
-robot.pos_y = 1
+robot.pos_x = START_ROBOT_POSITION_X
+robot.pos_y = START_ROBOT_POSITION_Y
 
-channel = Channel("amqp://guest:guest@localhost:5672")
+channel = Channel(AMQ_HOST)
 provider = ServiceProvider(channel)
 
 # Os tipos das mensagens devem ser passados, tanto no request como no reply
 provider.delegate(
-    topic="Locate.Robot",
+    topic=TOPIC_TO_LOCATE_ROBOT,
     function=get_position,
     request_type=Empty,
     reply_type=Position) 
 
 provider.delegate(
-    topic="Control.Robot",
+    topic=TOPIC_TO_CONTROL_ROBOT,
     function=set_position,
     request_type=Position,
     reply_type=Empty)
